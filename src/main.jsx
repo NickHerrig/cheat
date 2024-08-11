f48765f4bb076837381240cb61a79fc7aa4f8ca3
Nick Herrig <neherrig@gmail.com>
2024-08-10 20:46:15 -0400

    Scafold root route with index and other children routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root.jsx";
+import Root from "./routes/root.jsx";
+import Index, { loader as indexLoader } from "./routes/index.jsx";
 import Login, { action as loginAction } from "./routes/login.jsx";
 import Account, { loader as accountLoader } from "./routes/account.jsx";
 import { action as logoutAction } from "./routes/logout.jsx";
@@ -14,21 +15,27 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    loader: rootLoader,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    action: loginAction,
-  },
-  {
-    path: "/logout",
-    action: logoutAction,
-  },
-  {
-    path: "/account",
-    element: <Account />,
-    loader: accountLoader,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: indexLoader,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+        action: loginAction,
+      },
+      {
+        path: "/logout",
+        action: logoutAction,
+      },
+      {
+        path: "/account",
+        element: <Account />,
+        loader: accountLoader,
+      },
+    ],
   },
 ]);
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,36 +1,9 @@
-import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls } from '@react-three/drei'
-import { useLoaderData } from "react-router-dom"
-import { supabase } from "../supabaseClient"
-import Header from "../components/header"
-
-export async function loader() {
-  const { data, error } = await supabase.auth.getSession();
-  return data
-
-}
-
-function Model({ url }) {
-  const { scene } = useGLTF(url)
-  return <primitive object={scene} />
-}
+import { Outlet } from "react-router-dom"
 
 export default function Root() {
-  const { session } = useLoaderData()
-  // console.log(session.user.email)
   return (
-    <div className="min-h-screen flex flex-col bg-gray-900">
-      <Header session={session} />
-      <main className="flex-grow flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white mb-8">Let's pick your cheat meal!</h1>
-        <div className="w-full max-w-2xl h-96">
-          <Canvas camera={{ position: [0.5, 0.7, 0.2] }}>
-            <ambientLight intensity={.90} />
-            <Model url="/donut.glb" />
-            <OrbitControls />
-          </Canvas>
-        </div>
-      </main>
-    </div>
+    <>
+      <Outlet />
+    </>
   )
 }
